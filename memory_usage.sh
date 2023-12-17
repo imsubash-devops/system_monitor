f25e87eb6d363ec154ffe65025bdbed0592c6e93
whois_subash <vtu13626@veltech.edu.in>
2023-12-17 12:08:13 +0545

    added main.sh

--- memory_usage.sh
+++ memory_usage.sh
@@ -5,4 +5,7 @@
    Author: Subash
    Date: 17 Dec
 '
+echo "...................................."
+echo "............MEMORY USAGE............."
+echo "....................................."
 ps -eocomm,pmem | grep -E -v '(0.0)|(%mem)'
